289ab47b2935364c574c82496fbc56eb6ce43bd1
hyunhub <nuyhos_ambi@naver.com>
2024-11-04 02:37:13 +0900

    [append] windowOpen etc.

--- html/chapter10/ex10_1_windowOpen.js
+++ html/chapter10/ex10_1_windowOpen.js
@@ -1,32 +1,55 @@
-function onLoad ()
-{
-    // 객체 찾기.
-    const btnOpen = document.querySelector("#open");
-    const btnClose = document.querySelector("#close");
-    const idObj = document.querySelector("#userid");
-    const pwdObj = document.querySelector("#pwd");
-
+function onLoad(){
+    //객체찾기
+    const btnopen = document.querySelector("#open");
+    const btnclose = document.querySelector("#close");
+    const idobj = document.querySelector("#userid");
+    const pwd = document.querySelector("#pwd");
+    //팡업윈도우 === window 핸들변수
+    let win = null;  
     
-    // 팝업창(윈도우)의 윈도우 핸들 값. === window.
-    let win = null;
-
-    // 이벤트 리스너 등록 및 핸들러 처리.
-    btnOpen.addEventListener("click", () =>
-    {
-        // 윈도우 창 열기.
-        win = window.open("./ex8_2_formName.html", "_blank", "width=400, height=400, left=30, top=30");
-        setTimeout(() =>
-        {
-            win.document.querySelector("#userid").value = idObj.value;
-            win.document.querySelector("#pwd").value = pwdObj.value;
-        }, 100);
-        // const userID = win.document.querySelector("#userID");  // 8_2창임.
-        // userID.value = idObj.value;
+    //이벤트리스너등록및 핸들러처리
+    btnopen.addEventListener("click",()=>{
+        win = window.open("ex8_2_formName.html","_blank","width=400, height=400, left=100, top=100");
+        setTimeout(()=>{
+            win.document.querySelector("#userid").value = idobj.value; 
+            win.document.querySelector("#pwd").value = pwd.value; 
+        },100); 
+         
     });
-
-    btnClose.addEventListener("click", () =>
-    {
-        // 윈도우 창이 닫아짐. 그 창의 윈도우니까.
+    btnclose.addEventListener("click",()=>{
         win.close();
     });
 }
+
+// function onLoad ()
+// {
+//     // 객체 찾기.
+//     const btnOpen = document.querySelector("#open");
+//     const btnClose = document.querySelector("#close");
+//     const idObj = document.querySelector("#userid");
+//     const pwdObj = document.querySelector("#pwd");
+
+    
+//     // 팝업창(윈도우)의 윈도우 핸들 값. === window.
+//     let win = null;
+
+//     // 이벤트 리스너 등록 및 핸들러 처리.
+//     btnOpen.addEventListener("click", () =>
+//     {
+//         // 윈도우 창 열기.
+//         win = window.open("./ex8_2_formName.html", "_blank", "width=400, height=400, left=30, top=30");
+//         setTimeout(() =>
+//         {
+//             win.document.querySelector("#userid").value = idObj.value;
+//             win.document.querySelector("#pwd").value = pwdObj.value;
+//         }, 100);
+//         // const userID = win.document.querySelector("#userID");  // 8_2창임.
+//         // userID.value = idObj.value;
+//     });
+
+//     btnClose.addEventListener("click", () =>
+//     {
+//         // 윈도우 창이 닫아짐. 그 창의 윈도우니까.
+//         win.close();
+//     });
+// }
